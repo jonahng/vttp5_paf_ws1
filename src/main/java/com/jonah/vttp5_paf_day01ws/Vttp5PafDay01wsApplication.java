a7fc009b67fbf05429e8e225e6a60b39ba6f7e32
jonahng <nyhj1998@gmail.com>
2025-01-12 14:32:15 +0800

    configured imports

---
+++ src/main/java/com/jonah/vttp5_paf_day01ws/Vttp5PafDay01wsApplication.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_paf_day01ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5PafDay01wsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5PafDay01wsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonah/vttp5_paf_day01ws/Vttp5PafDay01wsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_paf_day01ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5PafDay01wsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
